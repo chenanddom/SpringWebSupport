7292e998c5e543730c8e332aefd1e2f68883c8c4
chendom <772571631@qq.com>
2018-12-29 09:56:39 +0800

    提交SpringBootWeb支持的代码...2

--- src/main/java/com/flexible/sws/SpringWebApplication.java
+++ src/main/java/com/flexible/sws/SpringWebApplication.java
@@ -18,27 +18,8 @@ import java.util.List;
  * Date: 2018-12-28
  * Time: 11:16
  */
-@Controller
 @SpringBootApplication
 public class SpringWebApplication {
-
-    @RequestMapping("/")
-    public String index(Model model){
-        Person single = new Person("aa",11);
-
-        List<Person> people = new ArrayList<Person>();
-        Person p1 = new Person("zhangsan",11);
-        Person p2 = new Person("lisi",22);
-        Person p3 = new Person("wangwu",33);
-        people.add(p1);
-        people.add(p2);
-        people.add(p3);
-
-        model.addAttribute("singlePerson", single);
-        model.addAttribute("people", people);
-
-        return "index";
-    }
 
     public static void main(String[] args) {
 //        ServerProperties
--- src/main/java/com/flexible/sws/controller/DemoController.java
+++ src/main/java/com/flexible/sws/controller/DemoController.java
@@ -19,5 +19,21 @@ import java.util.List;
  */
 @Controller
 public class DemoController {
+    @RequestMapping("/")
+    public String index(Model model){
+        Person single = new Person("aa",11);
 
+        List<Person> people = new ArrayList<Person>();
+        Person p1 = new Person("zhangsan",11);
+        Person p2 = new Person("lisi",22);
+        Person p3 = new Person("wangwu",33);
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+
+        model.addAttribute("singlePerson", single);
+        model.addAttribute("people", people);
+
+        return "index";
+    }
 }
